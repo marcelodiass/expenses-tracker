94d106467aea7402d950c64f8c55f2af13e27f3d
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-02-22 15:54:43 -0300

    Routes configuration + deleteIncome method.

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -37,4 +37,14 @@ exports.getIncomes = async (req, res) => {
   }
 }
 
-
+exports.deleteIncome = async (req, res) => {
+  const {id} = req.params
+  
+  IncomeSchema.findByIdAndDelete(id)
+    .then(() => {
+      res.status(200).json({message: "Income Deleted"})
+    })
+    .catch(() => {
+      res.status(500).json({message: "Server Error"})
+    })
+}

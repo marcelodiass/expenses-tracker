f3d3ce339b57203b464eb8f9d32155534934aace
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-02-22 15:29:11 -0300

    getIncomes method added.

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -27,3 +27,14 @@ exports.addIncome = async (req, res) => {
     res.status(500).json({message: "Server Error"})
   }
 };
+
+exports.getIncomes = async (req, res) => {
+  try {
+    const incomes = await IncomeSchema.find().sort({createdAt: -1})
+    res.status(200).json(incomes)
+  } catch (error) {
+    res.status(500).json({message: "Server Error"})
+  }
+}
+
+

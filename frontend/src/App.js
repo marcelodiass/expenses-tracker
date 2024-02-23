1fd2fa5ae94c86431d953591c454ccb20fdd2389
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-02-22 23:29:33 -0300

    Background Image.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import styled from "styled-components";
+import bg from "./img/bg.png";
+
 function App() {
+  
   return (
-    <div className="App">
-    </div>
+    <AppStyled bg={bg} className="App">
+      <main>
+        
+      </main>
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+    height: 100vh;
+    background-image: url(${props => props.bg});
+    position: relative;
+  `;
 
 export default App;

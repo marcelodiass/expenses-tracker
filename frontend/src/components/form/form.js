f5907bfc6fd0dee4548e0ac804383bbc4b0d0970
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-02-26 23:10:51 -0300

    feat: finish Incomes section.

--- frontend/src/components/form/form.js
+++ frontend/src/components/form/form.js
@@ -26,7 +26,13 @@ function Form() {
   const handleSubmit = (e) => {
     e.preventDefault();
     addIncome(inputState);
-    getIncomes()
+    setInputState({
+      title: "",
+      amount: "",
+      date: "",
+      category: "",
+      description: ""
+    })
   };
 
   return (

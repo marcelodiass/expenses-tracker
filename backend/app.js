279cc5ebc5456ad6d371f09367a2e4167e2d7579
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-03-01 13:48:45 -0300

    fix: deploy 2

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 // middlewares
 app.use(express.json());
 app.use(cors({
-  origin: ["https://expenses-tracker-backend.vercel.app"],
+  origin: ["https://expenses-tracker-backend-4hn7w5cze.vercel.app"],
   methods: ["POST", "GET", "DELETE"],
   credentials: true
 }));

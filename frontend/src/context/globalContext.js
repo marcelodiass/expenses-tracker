61074e65289211b6bbd6efb4494768ba1f0d8957
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-03-01 13:05:50 -0300

    feat: dashboard styles

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -81,7 +81,7 @@ export const GlobalProvider = ({children}) => {
             return new Date(b.createdAt) - new Date(a.createdAt)
         })
 
-        return history
+        return history.slice(0,3)
     }
 
 

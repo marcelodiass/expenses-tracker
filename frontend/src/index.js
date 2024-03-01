935d77aa27cd6d114815b85e792b0e823d8a5c00
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-03-01 19:12:20 -0300

    Ready to deploy frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { GlobalProvider } from "./context/globalContext";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
